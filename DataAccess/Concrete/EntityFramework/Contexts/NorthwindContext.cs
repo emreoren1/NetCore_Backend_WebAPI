27339863a26c94415d62721519735e4b171e1993
Emre <emreoren1905@gmail.com>
2020-04-23 19:17:25 +0300

    Core ve DataAccess katmanlarında bazı eklemeler yapıldı. Context nesnesi oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework.Contexts
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= DESKTOP-4BNEQHM; Database=Northwind;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
